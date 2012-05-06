e5134b5918d562e98f4efe66d6d0973e1e9cd0b7
Nuno Correia <ncorreia@ipn.pt>
2012-05-06 02:17:20 +0100

    new project

--- spotilicious/gen/com/spotilicious/R.java
+++ spotilicious/gen/com/spotilicious/R.java
@@ -11,13 +11,27 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int eight=0x7f020000;
+        public static final int five=0x7f020001;
+        public static final int four=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int nine=0x7f020004;
+        public static final int one=0x7f020005;
+        public static final int seven=0x7f020006;
+        public static final int six=0x7f020007;
+        public static final int three=0x7f020008;
+        public static final int two=0x7f020009;
+        public static final int zero=0x7f02000a;
     }
     public static final class id {
-        public static final int myGrid=0x7f050000;
+        public static final int GridItem=0x7f050000;
+        public static final int grid_item_image=0x7f050002;
+        public static final int grid_item_text=0x7f050001;
+        public static final int gridview=0x7f050003;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int grid_item=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- spotilicious/src/com/spotilicious/spotilicious.java
+++ spotilicious/src/com/spotilicious/spotilicious.java
@@ -1,66 +1,57 @@
 package com.spotilicious;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.BaseAdapter;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.GridView;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 public class spotilicious extends Activity {
+	private CustomAdapter mAdapter;
+	private ArrayList<String> mItems = new ArrayList<String>();
+	
 	@Override
-	   public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.main);
-	        GridView g = (GridView) findViewById(R.id.myGrid);
-	        g.setAdapter(new ImageAdapter(this));
-	        g.setOnItemClickListener(new OnItemClickListener() {
-	           public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-	              Toast.makeText(spotilicious.this, "" + position, Toast.LENGTH_SHORT).show();
-	           }
-	        });
-	   }
-	   public class ImageAdapter extends BaseAdapter {
-	        public ImageAdapter(Context c) {
-	                mContext = c;
-	        }
-	        public int getCount() {
-	                return mThumbIds.length;
-	        }
-	        public Object getItem(int position) {
-	                return position;
-	        }
-	        public long getItemId(int position) {
-	                return position;
-	        }
-	        public View getView(int position, View convertView, ViewGroup parent) {
-	                ImageView imageView;
-	                if (convertView == null) {
-	                   imageView = new ImageView(mContext);
-	                   imageView.setLayoutParams(new GridView.LayoutParams(45, 45));
-	                   imageView.setAdjustViewBounds(false);
-	                   imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-	                   imageView.setPadding(8, 8, 8, 8);
-	                } else {
-	                   imageView = (ImageView) convertView;
-	                }
-	                imageView.setImageResource(mThumbIds[position]);
-	                return imageView;
-	        }
-	        private Context mContext;
-	        private Integer[] mThumbIds = {
-	                R.drawable.icon, R.drawable.icon,
-	                R.drawable.icon, R.drawable.icon,
-	                R.drawable.icon, R.drawable.icon,
-	                R.drawable.icon, R.drawable.icon,
-	                R.drawable.icon, R.drawable.icon,
-	                R.drawable.icon, R.drawable.icon,
-	                R.drawable.icon, R.drawable.icon,
-	        };
-	    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		for (int i = 0; i<10; i++) {
+			mItems.add(Integer.toString(i));
+		}
+	
+		mAdapter = new CustomAdapter(this, mItems);
+		GridView g = (GridView) findViewById(R.id.gridview);
+		g.setAdapter(mAdapter);
+		registerForContextMenu(g);
+	}
+	
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		menu.setHeaderTitle("Context Menu");
+		AdapterContextMenuInfo cmi = (AdapterContextMenuInfo) menuInfo;
+		menu.add(1, cmi.position, 0, "Action 1");
+		menu.add(2, cmi.position, 0, "Action 2");
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		GridView g = (GridView) findViewById(R.id.gridview);
+		String s = (String) g.getItemAtPosition(item.getItemId());
+		switch (item.getGroupId()) {
+		case 1:
+		Toast.makeText(this, "Action 1, Item "+s, Toast.LENGTH_SHORT).show();
+		break;
+		case 2:
+		Toast.makeText(this, "Action 2, Item "+s, Toast.LENGTH_SHORT).show();
+		break;
+		}
+		return true;
+	}
 }
