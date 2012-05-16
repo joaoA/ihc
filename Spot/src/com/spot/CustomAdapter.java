adc3c3954446ebed1e3bf79c2348332b627f3e06
joao <amsellem@student.dei.uc.pt>
2012-05-16 03:29:15 +0100

    novo projecto

---
+++ Spot/src/com/spot/CustomAdapter.java
@@ -0,0 +1,65 @@
+package com.spot;
+
+
+import java.util.ArrayList;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+public class CustomAdapter extends BaseAdapter {
+	private Context mContext;
+	private ArrayList<String> mItems;
+	public CustomAdapter(Context c, ArrayList<String> items) {
+	mContext = c;
+	mItems = items;
+	}
+	public int getCount() {
+	return mItems.size();
+	}
+	public Object getItem(int position) {
+	return mItems.get(position);
+	}
+	
+	public long getItemId(int position) {
+	return position;
+	}
+	
+	public View getView(int position, View convertView, ViewGroup parent) {
+	View v = convertView;
+	if (v == null) {
+		LayoutInflater li = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		v = li.inflate(R.layout.grid_item, null);
+		TextView tv = (TextView)v.findViewById(R.id.grid_item_text);
+		tv.setText(mItems.get(position));
+		ImageView iv = (ImageView)v.findViewById(R.id.grid_item_image);
+		switch (position) {
+		case 0:
+			iv.setImageResource(R.drawable.ic_launcher);
+			break;
+		case 1:
+			iv.setImageResource(R.drawable.ic_launcher);
+			break;
+		case 2:
+			iv.setImageResource(R.drawable.ic_launcher);
+			break;
+		case 3:
+			iv.setImageResource(R.drawable.ic_launcher);
+			break;
+		case 4:
+			iv.setImageResource(R.drawable.ic_launcher);
+			break;
+		case 5:
+			iv.setImageResource(R.drawable.ic_launcher);
+			break;
+		case 6:
+			iv.setImageResource(R.drawable.ic_launcher);
+			break;
+		
+		}
+	}
+	return v;
+	}
+}
---
+++ Spot/src/com/spot/Spotilicious.java
@@ -0,0 +1,63 @@
+package com.spot;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class Spotilicious extends Activity {
+	private CustomAdapter mAdapter;
+	private ArrayList<String> mItems = new ArrayList<String>();
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		mItems.add("Procura por proximidade");
+		mItems.add("Procura por cozinha");
+		mItems.add("Procura por preço");
+		mItems.add("Procura multimodal");
+		mItems.add("Histórico");
+		mItems.add("Favoritos");
+		
+	
+		mAdapter = new CustomAdapter(this, mItems);
+		GridView g = (GridView) findViewById(R.id.gridview);
+		g.setAdapter(mAdapter);
+		registerForContextMenu(g);
+	}
+	
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		menu.setHeaderTitle("Context Menu");
+		AdapterContextMenuInfo cmi = (AdapterContextMenuInfo) menuInfo;
+		menu.add(1, cmi.position, 0, "Action 1");
+		menu.add(2, cmi.position, 0, "Action 2");
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		GridView g = (GridView) findViewById(R.id.gridview);
+		String s = (String) g.getItemAtPosition(item.getItemId());
+		switch (item.getGroupId()) {
+		case 1:
+		Toast.makeText(this, "Action 1, Item "+s, Toast.LENGTH_SHORT).show();
+		break;
+		case 2:
+		Toast.makeText(this, "Action 2, Item "+s, Toast.LENGTH_SHORT).show();
+		break;
+		}
+		return true;
+	}
+}
